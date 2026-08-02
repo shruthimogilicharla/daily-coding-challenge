601829c3d5395e3049d0ae4546f4424577986ae3
shruthimogilicharla <shruthimogilicharla02@gmail.com>
2026-08-02 21:22:53 +0530

    Day 11 - Singly Linked List in C

---
+++ Day11/singly_linked_list.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+
+void insert(int x) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = head;
+    head = newNode;
+}
+
+void display() {
+    struct Node *temp = head;
+    printf("Linked List: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    insert(30);
+    insert(20);
+    insert(10);
+
+    display();
+
+    return 0;
+}
