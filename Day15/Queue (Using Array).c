b81cb460d4769920efedb9ef48e51f50e651b763
shruthimogilicharla <shruthimogilicharla02@gmail.com>
2026-08-04 21:54:06 +0530

    Day 15 - Added Queue (Using Array) in C

---
+++ Day15/Queue (Using Array).c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#define MAX 5
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int value) {
+    if (rear == MAX - 1) {
+        printf("Queue Overflow\n");
+        return;
+    }
+    if (front == -1)
+        front = 0;
+    queue[++rear] = value;
+}
+
+void dequeue() {
+    if (front == -1 || front > rear) {
+        printf("Queue Underflow\n");
+        return;
+    }
+    printf("Deleted: %d\n", queue[front++]);
+
+    if (front > rear)
+        front = rear = -1;
+}
+
+void display() {
+    if (front == -1) {
+        printf("Queue is Empty\n");
+        return;
+    }
+
+    printf("Queue: ");
+    for (int i = front; i <= rear; i++)
+        printf("%d ", queue[i]);
+    printf("\n");
+}
+
+int main() {
+    int choice, value;
+
+    while (1) {
+        printf("\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &value);
+                enqueue(value);
+                break;
+
+            case 2:
+                dequeue();
+                break;
+
+            case 3:
+                display();
+                break;
+
+            case 4:
+                return 0;
+
+            default:
+                printf("Invalid Choice\n");
+        }
+    }
+}
