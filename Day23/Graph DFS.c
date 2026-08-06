72804ab4ecfdf4c49acb8ab5ad24fb5fa5f0cbed
shruthimogilicharla <shruthimogilicharla02@gmail.com>
2026-08-06 20:36:26 +0530

    Day 23 - Added Graph DFS in C

---
+++ Day23/Graph DFS.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int graph[10][10], visited[10], n;
+
+void dfs(int v)
+{
+    visited[v] = 1;
+    printf("%d ", v);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (graph[v][i] == 1 && visited[i] == 0)
+            dfs(i);
+    }
+}
+
+int main()
+{
+    int start;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &graph[i][j]);
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &start);
+
+    printf("DFS Traversal: ");
+    dfs(start);
+
+    return 0;
+}
